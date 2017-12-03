2d33b0510405aa9f9a5e611894246071da762b9e
Unknown <david.heff.dh@googlemail.com>
2017-12-03 15:37:59 +0000

    firebae log in working

--- app/src/main/java/com/artesseum/hi5/MainActivity.java
+++ app/src/main/java/com/artesseum/hi5/MainActivity.java
@@ -1,38 +1,101 @@
 package com.artesseum.hi5;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final int RC_SIGN_IN = 123;
+    private FirebaseAuth auth;
 
 
-    private DatabaseReference mDatabase;
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==RC_SIGN_IN){
+            if (resultCode == RESULT_OK){
+                // user logged in
+                Log.d("AUTH", auth.getCurrentUser().getEmail());
+            }
+            else{
+                //// user auth
+                Log.d("Auth", "Not Authenticated");
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference().child("Message");
 
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+
+
+        auth = FirebaseAuth.getInstance();
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+
+
+        } else {
+
+
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.Builder(AuthUI.EMAIL_PROVIDER).build(),
+                    new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build(),
+                    new AuthUI.IdpConfig.Builder(AuthUI.FACEBOOK_PROVIDER).build());
+
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(), RC_SIGN_IN);
+        }
+
+
+        findViewById(R.id.signout).setOnClickListener(this);
 
 
     }
 
 
-    public void sendButtonClick(View view){
-        final DatabaseReference newPost = mDatabase.push();
-        newPost.child("content").setValue("Hi Five!");
-
-
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.signout){
+            AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                finish();
+                Log.d("Auth","User Logged Out");
+                }
+            });
+        }
 
     }
-
-
-
 }
+
