e566320646169164bade8af9187e5bfeb21fdc63
Unknown <david.heff.dh@googlemail.com>
2017-12-23 15:10:20 +0000

    search user
    populates view holder.

--- app/src/main/java/com/artesseum/hi5/SearchActivity.java
+++ app/src/main/java/com/artesseum/hi5/SearchActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -28,6 +30,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import org.w3c.dom.Text;
 
@@ -35,82 +38,83 @@ public class SearchActivity extends AppCompatActivity {
 
     public static String finalSearchText="";
     public static String senderUSerID="";
-    public static String receiverUserId="";
-    private String CurrentState;
     private RecyclerView usersList;
+    FirebaseRecyclerAdapter adapter;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        startQuery();
+        usersList = (RecyclerView) findViewById(R.id.useresRecycleView);
+        usersList.setHasFixedSize(true);
+        usersList.setLayoutManager(new LinearLayoutManager(this));
 
 
-        CurrentState="not_friends";
+
+
+
         String senderID = FirebaseAuth.getInstance().getCurrentUser().getUid();
         senderUSerID = senderID;
+        final EditText searchText = (EditText)findViewById(R.id.searchText);
 
-        Button searchButton = (Button)findViewById(R.id.searchButton);
 
-       // startQuery();
 
-            searchButton.setOnClickListener(new View.OnClickListener() {
+
+
+
+
+        searchText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void onClick(View view) {
-                startQuery();
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                try{
+                    String searchTextFinal = searchText.getText().toString();
+                    finalSearchText=searchTextFinal;
+                    startQuery();
+
+                }catch (Error e){
+
+                }            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                try{
+                    String searchTextFinal = searchText.getText().toString();
+                    finalSearchText=searchTextFinal;
+                    startQuery();
+
+                }catch (Error e){
+
+                }
+
 
             }
         });
 
-
-
-
     }
 
+
     private void startQuery() {
-        getSearchText();
-
-        Query query = FirebaseDatabase.getInstance().getReference().child("users").child("displayname");
 
 
-        ChildEventListener childEventListener = new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        };
-        query.addChildEventListener(childEventListener);
-
+        Query query = FirebaseDatabase.getInstance().getReference().child("users").orderByChild("displayname").equalTo(finalSearchText);
         FirebaseRecyclerOptions<Users> options =
                 new FirebaseRecyclerOptions.Builder<Users>().setQuery(query,Users.class)
                         .build();
-
-
-        FirebaseRecyclerAdapter adapter = new FirebaseRecyclerAdapter<Users, usersHolder>(options) {
+        adapter = new FirebaseRecyclerAdapter<Users, usersHolder>(options) {
             @Override
             public usersHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
                 View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_profile,parent,false);
+
                 return new usersHolder(view);
 
             }
@@ -118,8 +122,8 @@ public class SearchActivity extends AppCompatActivity {
             protected void onBindViewHolder(usersHolder holder, int position, Users model) {
 
                 holder.setDisplayname(model.getDisplayname());
-                holder.setDisplayname(model.getEmail());
-                holder.setDisplayname(model.getPhotoURL());
+                holder.setEmail(model.getEmail());
+                holder.setPhotoURL(model.getPhotoURL());
 
 
             }
@@ -131,24 +135,12 @@ public class SearchActivity extends AppCompatActivity {
       usersList.setHasFixedSize(true);
       usersList.setLayoutManager(new LinearLayoutManager(this));
       usersList.setAdapter(adapter);
-
-
-
-
-
-
+      adapter.startListening();
 
     }
 
 
-    private void getSearchText() {
 
-        EditText searchText = (EditText)findViewById(R.id.searchText);
-        String searchTextFinal = searchText.getText().toString();
-        finalSearchText=searchTextFinal;
-
-
-    }
 
     public static class usersHolder extends RecyclerView.ViewHolder{
         TextView username, email;
@@ -163,6 +155,17 @@ public class SearchActivity extends AppCompatActivity {
         public void setDisplayname(String displayname){
             TextView username = (TextView) itemView.findViewById(R.id.displaynameProfileView);
             username.setText(displayname);
+        }
+
+        public void setEmail(String email){
+            TextView emailview = (TextView) itemView.findViewById(R.id.userEmailDisplay);
+            emailview.setText(email);
+        }
+
+        public void setPhotoURL(String photoURL){
+            ImageView profilepicview = (ImageView) itemView.findViewById(R.id.userProfilePic);
+            String umageURI = photoURL;
+            Picasso.with(itemView.getContext()).load(photoURL).into(profilepicview);
         }
 
 
